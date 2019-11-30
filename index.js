50fb59605e42e33edcb9adc0365e864b20f42a36
Bry Cox <carl.cc@gmail.com>
2019-11-30 07:52:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,42 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(obj, fn) {
+      for(const value of Object.values(obj)) {
+        fn(value);
+      };
+      return obj;
     },
 
-    map: function() {
-
+    map: function(obj, fn) {
+      const newArr = [];
+      for(const value of Object.values(obj)) {
+        newArr.push(fn(value));
+      };
+      return newArr
     },
 
-    reduce: function() {
-
+    reduce: function(obj, fn, accumulator) {
+      let i = 0;
+      const array = Object.values(obj);
+      if(!accumulator) {
+        accumulator = array[0];
+        i = 1;
+      };
+      for(; i < array.length; i++) {
+        accumulator = fn(accumulator, array[i]);
+      };
+      return accumulator;
     },
 
-    functions: function() {
+    find: (obj, fn) => { for(const value of Object.values(obj)) { if(fn(value)) { return value } } },
 
+    filter: (obj, fn) => {
+      const newArr = [];
+      for(const value of Object.values(obj)) {
+        if(fn(value)) { newArr.push(value) };
+      };
+      return newArr;
     },
 
 
